ceb623d46795aae75c4c2f4ae7bb4d8b17337e76
Karla Alves <karlaayeska12@gmail.com>
2018-03-02 15:32:11 -0700

    one more

--- team_board.js
+++ team_board.js
@@ -14,51 +14,52 @@ fixture `Team Board`
     //     await t.eval(() => localStorage.clear());
     // })
 
-    // test('Team Board Exit Agreements', async t =>{
-    //     const moreButton = Selector('div.chr-TeamBoardBoardToolbar-moreButton')
-    //     const showExitAgreements = Selector('li.smb-DropdownItem')
-    //     const hideExitAgreements = Selector('li.smb-DropdownItem')
-    //     const exitAgreementFieldText = Selector('.smb-FieldLabel-text')
+
+    test('Team Board Exit Agreements', async t =>{
+        const moreButton = Selector('div.chr-TeamBoardBoardToolbar-moreButton')
+        const showExitAgreements = Selector('li.smb-DropdownItem')
+        const hideExitAgreements = Selector('li.smb-DropdownItem')
+        const exitAgreementFieldText = Selector('.smb-FieldLabel-text')
         
 
-    //     await t
-    //         .click(moreButton)
-    //         .click(showExitAgreements)
-    //         .expect(showExitAgreements).ok('it passed')
-    //         .wait(2000)
-    //         if(await exitAgreementFieldText.exists){
-    //             await t
-    //                 .click(moreButton)
-    //                 .click(hideExitAgreements)
-    //                 .wait(2000)
-    //         }else{
-    //             await t.expect(true).ok('it passed')
-    //         }
+        await t
+            .click(moreButton)
+            .click(showExitAgreements)
+            .expect(showExitAgreements).ok('it passed')
+            .wait(2000)
+            if(await exitAgreementFieldText.exists){
+                await t
+                    .click(moreButton)
+                    .click(hideExitAgreements)
+                    .wait(2000)
+            }else{
+                await t.expect(true).ok('it passed')
+            }
 
-    // }) 
+    }) 
 
-    // test('Team Board settings, adding New Flow State', async t =>{
-    //     const settingsButton = Selector('.chr-TeamBoardPage-settingsViewButton')
-    //     const addFlowState = Selector('.chr-TeamBoardSettings-addColumn')
-    //     const nameContainer = Selector('.chr-TeamBoardSettingsColumn-nameContainer')
-    //     const saveButton = Selector('button.smb-Button.smb-Button--primary.smb-Button--sm')
-    //     const dropdownItemFlowState = Selector('.chr-TeamBoardSettingsColumn-moreDropdown')
-    //     const deleteFlowState = Selector('span.smb-DropdownItem-text').withText('Delete');
+    test('Team Board settings, adding New Flow State', async t =>{
+        const settingsButton = Selector('.chr-TeamBoardPage-settingsViewButton')
+        const addFlowState = Selector('.chr-TeamBoardSettings-addColumn')
+        const nameContainer = Selector('.chr-TeamBoardSettingsColumn-nameContainer')
+        const saveButton = Selector('button.smb-Button.smb-Button--primary.smb-Button--sm')
+        const dropdownItemFlowState = Selector('.chr-TeamBoardSettingsColumn-moreDropdown')
+        const deleteFlowState = Selector('span.smb-DropdownItem-text').withText('Delete');
 
-    //     await t
-    //         .click(settingsButton)
-    //         .wait(2000)
-    //         .click(addFlowState)
-    //         .expect(addFlowState).ok('it passed')
-    //         .typeText(nameContainer, 'New Flow 1')
-    //         .click(saveButton)
-    //         .wait(2000)
-    //         //.expect(nameContainer.value).to.eql('New Flow 1')
-    //         .click(dropdownItemFlowState)
-    //         .click(deleteFlowState)
-    //         .expect(deleteFlowState).ok('it passed')
+        await t
+            .click(settingsButton)
+            .wait(2000)
+            .click(addFlowState)
+            .expect(addFlowState).ok('it passed')
+            .typeText(nameContainer, 'New Flow 1')
+            .click(saveButton)
+            .wait(2000)
+            //.expect(nameContainer.value).to.eql('New Flow 1')
+            .click(dropdownItemFlowState)
+            .click(deleteFlowState)
+            .expect(deleteFlowState).ok('it passed')
 
-    // })
+    })
 
     test('Team Board add US, setReady, setBlocked with a reason,drag and delete new artifact', async t =>{
         const addNewButton = Selector('span.smb-Button-children').withText('Add New')
@@ -87,6 +88,7 @@ fixture `Team Board`
             .click(createUserStory)
             .typeText(enterName, 'User Story 1')
             .click(createButton)
+            .expect({a:'User Story 1'}).eql({a:'User Story 1'}, 'this assertion will pass')
 
             //Drag User Story
             .drag(dragUs, 360,0,{offsetX: 10, offsetY:10})
@@ -94,6 +96,7 @@ fixture `Team Board`
 
             //Set User Story as Ready
             .click(setReady)
+            //.expect(setReady.checked).ok()
 
             //Set User Story as Blocked with Blocked Reason
             .click(setBlocked)
@@ -106,12 +109,44 @@ fixture `Team Board`
             .wait(2000)
             .click(applyField)
 
+    
             //Open qdp and Delete User story
             .click(formattedId)
             .click(qdpMoreButton)
             .click(qdpDelete)
             .click(flairConfirmDeletion)
             
+    })
+
+    test('Card inline edit', async t =>{
+        const storyName = Selector('.chr-BoardField--editable').withText('User Story 1')
+        const addNewButton = Selector('span.smb-Button-children').withText('Add New')
+        const workItemTypes = Selector('div.smb-Select-placeholderText').withText('Select Work Item Types...')
+        const createUserStory = Selector('span.smb-DropdownItem-text').withText('User Story')
+        const enterName = Selector('.smb-TextInput--iconPlacementEnd')
+        const createButton = Selector('.chr-AddNewTray-addButton')
+        const inlineEditor = Selector('.chr-EditorsStringEditor')
+        const formattedId = Selector('.chr-FormattedId')
+        const qdpMoreButton = Selector('.chr-QuickDetailActionsMenu')
+        const qdpDelete = Selector('.smb-DropdownItem-text').withText('Delete')
+        const flairConfirmDeletion = Selector('.smb-Button-children').withText('Delete')
+        
+        
+        await t 
+            //create an User Story        
+            .click(addNewButton)
+            .click(workItemTypes)
+            .click(createUserStory)
+            .typeText(enterName, 'User Story 1')
+            .click(createButton)
+            .wait(2000)
+            .hover(storyName).click(storyName)
+            .typeText(inlineEditor, ' Test')
+            .pressKey('enter')
+            .click(formattedId)
+            .click(qdpMoreButton)
+            .click(qdpDelete)
+            .click(flairConfirmDeletion)
 
     })
             
