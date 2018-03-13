0387080df5995c6ad5d8ea93b62a13ccc5167960
Karla Alves <karlaayeska12@gmail.com>
2018-03-13 17:23:01 -0600

    save us

--- user_story_tps.js
+++ user_story_tps.js
@@ -11,22 +11,43 @@ fixture `User Stories Page`
     .page(`https://karla0.testn.f4tech.com/#/20330408691d/userstories`)  
     .beforeEach(login)
 
-    test('User Story', async t =>{
-        const newUserStory = Selector(value => {
-            return document.getElementById('newAR')
+    // test('User Story - New User Story', async t =>{
+    //     const newUserStory = Selector(value => {
+    //         return document.getElementById('newAR')
+    //     })
+    //     const newUsName = Selector('div.smb-TextInput-renderedText').nth(0)
+    //     const newUsNameEditor = Selector('.smb-TextInput--iconPlacementEnd').nth(0)
+    //     const qdpMoreButton = Selector('.chr-QuickDetailActionsMenu')
+    //     const qdpDelete = Selector('.smb-DropdownItem-text').withText('Delete')
+    //     const flairConfirmDeletion = Selector('.smb-Button-children').withText('Delete')
+    //     await t
+    //         .hover('.actionsMenu')
+    //         .wait(2000)
+    //         .click(newUserStory)
+    //         .click(newUsName)
+    //         .typeText(newUsNameEditor, 'New US')
+    //         .click('.chr-QuickDetailEntityFooter-saveButton')
+    //         .wait(2000)
+    //         .expect({input:'New US'}).eql({input:'New US'}, 'this assertion will pass')
+    //         .click(qdpMoreButton)
+    //         .click(qdpDelete)
+    //         .click(flairConfirmDeletion)
+    //         .wait(2000)       
+    // })
+
+    test('User Story - Inline Add', async t =>{
+        const inlineAddStory = Selector(value =>{
+            return document.getElementById('inline')
         })
-        const newUsName = Selector('div.smb-TextInput-renderedText').nth(0)
-        const newUsNameEditor = Selector('.smb-TextInput--iconPlacementEnd').nth(0)
+        const inlineAddName = Selector('.editrow').nth(4)
+
         await t
             .hover('.actionsMenu')
+            .click(inlineAddStory)
             .wait(2000)
-            .click(newUserStory)
-            .click(newUsName)
-            .typeText(newUsNameEditor, 'New US')
-            .click('.chr-QuickDetailEntityFooter-saveButton')
+            .typeText(inlineAddName, 'US A')
             .wait(2000)
-            .click('.smb-Icon--cancel')
-            .expect({input:'New US'}).eql({input:'New US'}, 'this assertion will pass')       
+            .click('.save')
     })
 
     
