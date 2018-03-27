9b2a6d2bcd241b06a267d384fb329da95274f074
Karla Alves <karlaayeska12@gmail.com>
2018-03-27 13:56:07 -0600

    more us tests

--- user_story_tps.js
+++ user_story_tps.js
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 
 var login = async t => {
     await t
@@ -6,10 +6,16 @@ var login = async t => {
         .typeText('#j_password', 'Password')
         .click('#login-button')        
 }
+
+// var current_page = async t=> {
+//     await t
+//         .eval(() => window.location.reload(true))
+// }
     
 fixture `User Stories Page`
     .page(`https://karla0.testn.f4tech.com/#/20330408691d/userstories`)  
     .beforeEach(login)
+    //.afterEach(current_page)
 
     test('User Story - New User Story', async t =>{
         const newUserStory = Selector(value => {
@@ -35,50 +41,50 @@ fixture `User Stories Page`
             .wait(2000)       
     })
 
-    // test('User Story - Inline Add', async t =>{
-    //     const inlineAddStory = Selector(value =>{
-    //         return document.getElementById('inline')
-    //     })
-    //     const inlineAddName = Selector('.editrow').nth(4)
-    //     const formattedId = Selector('.cn_formattedid0').nth(0)
-    //     const qdpMoreButton = Selector('.chr-QuickDetailActionsMenu')
-    //     const qdpDelete = Selector('.smb-DropdownItem-text').withText('Delete')
-    //     const flairConfirmDeletion = Selector('.smb-Button-children').withText('Delete')
+    test('User Story - Inline Add', async t =>{
+        const inlineAddStory = Selector(value =>{
+            return document.getElementById('inline')
+        })
+        const inlineAddName = Selector('.editrow').nth(4)
+        const formattedId = Selector('.cn_formattedid0').nth(0)
+        const qdpMoreButton = Selector('.chr-QuickDetailActionsMenu')
+        const qdpDelete = Selector('.smb-DropdownItem-text').withText('Delete')
+        const flairConfirmDeletion = Selector('.smb-Button-children').withText('Delete')
 
-    //     await t
-    //         .hover('.actionsMenu')
-    //         .click(inlineAddStory)
-    //         .wait(2000)
-    //         .typeText(inlineAddName, 'US A')
-    //         .wait(2000)
-    //         .click('.save')
-    //         //expect story was created goes here????
-    //         .click(formattedId)
-    //         .click(qdpMoreButton)
-    //         .click(qdpDelete)
-    //         .click(flairConfirmDeletion)
+        await t
+            .hover('.actionsMenu')
+            .click(inlineAddStory)
+            .wait(2000)
+            .typeText(inlineAddName, 'US A')
+            .wait(2000)
+            .click('.save')
+            //expect story was created goes here????
+            .click(formattedId)
+            .click(qdpMoreButton)
+            .click(qdpDelete)
+            .click(flairConfirmDeletion)
 
-    // })
+    })
 
-    // test('User Story - Multi Edit', async t =>{
-    //     const multiEdit = Selector(value =>{
-    //         return document.getElementById('multi')
-    //     })
-    //     const usState = Selector('.hover').withText('In-Progress')
-    //     const applyButton = Selector('button').withText('Apply')
-    //     const saveChanges = Selector('button').withText('Save Changes')
+    test('User Story - Multi Edit', async t =>{
+        const multiEdit = Selector(value =>{
+            return document.getElementById('multi')
+        })
+        const usState = Selector('.hover').withText('In-Progress')
+        const applyButton = Selector('button').withText('Apply')
+        const saveChanges = Selector('button').withText('Save Changes')
 
-    //     await t
-    //         .hover('.actionsMenu')
-    //         .click(multiEdit)
-    //         .click('#nameTemplate')
-    //         .typeText('#nameTemplate', 'New Name')
-    //         .click(applyButton)
-    //         .hover('.state-table')
-    //         .click('.state-table')
-    //         .wait(2000)
-    //         .click(saveChanges)
-    // })
+        await t
+            .hover('.actionsMenu')
+            .click(multiEdit)
+            .click('#nameTemplate')
+            .typeText('#nameTemplate', 'New Name')
+            .click(applyButton)
+            .hover('.state-table')
+            .click('.state-table')
+            .wait(2000)
+            .click(saveChanges)
+    })
 
 
     test('User Story - Inline Add, Multi - Edit', async t =>{
@@ -123,20 +129,62 @@ fixture `User Stories Page`
 
     })
 
-    // test('Add Child Story', async t =>{
-    //     const inlineAddStory = Selector(value =>{
-    //         return document.getElementById('inline')
-    //     })
-    //     const inlineAddName = Selector('.editrow').nth(4)
+    test('Add Child Story', async t =>{
+        const inlineAddStory = Selector(value =>{
+            return document.getElementById('inline')
+        })
+                    
+        const inlineAddName = Selector('.editrow').nth(4)
+        const newUsName = Selector('div.smb-TextInput-renderedText').nth(0)
+        const newUsNameEditor = Selector('.smb-TextInput--iconPlacementEnd').nth(0)
+        const closeIconQDP = Selector('.smb-Icon--cancel')
+        const formattedId = Selector('.cn_formattedid0').nth(0)
+        const qdpMoreButton = Selector('.chr-QuickDetailActionsMenu')
+        const qdpDelete = Selector('.smb-DropdownItem-text').withText('Delete')
+        const flairConfirmDeletion = Selector('.smb-Button-children').withText('Delete')
+        const listView = Selector('#flatmode')
+        const treeView = Selector('#hiermode')
+        const expandChildIcon = Selector('.icon_plus').nth(1)
 
-    //     await t
-    //         .hover('.actionsMenu')
-    //         .click(inlineAddStory)
-    //         .wait(2000)
-    //         .typeText(inlineAddName, 'US A')
-    //         .wait(2000)
-    //         .click('.save')
-    //         .click('.sprite-new-story-child')
+        await t
+            .hover('.actionsMenu')
+            .click(inlineAddStory)
+            .wait(2000)
+            .typeText(inlineAddName, 'US A') 
+            .wait(2000)
+            .click('.save')
+            .click('.sprite-new-story-child')
+            .click(newUsName)
+            .typeText(newUsNameEditor, 'New US 1')
+            .click('.chr-QuickDetailEntityFooter-saveButton')
+            .click(closeIconQDP)
+            .wait(2000)
+            .click(listView)
+            .click(treeView)
+            .expect(expandChildIcon.exists).ok('it passed')
+            .wait(2000)
+            .click(formattedId)
+            .click(qdpMoreButton)
+            .click(qdpDelete)
+            .click(flairConfirmDeletion)
+            //.navigateTo(current_page)
+                  
+    })
 
+    //A story with a child needs to exist for this test to pass
+    test('User Stories expand/collapse functionality', async t => {
 
-    // })
+        const expandPageIcon = Selector('img.icon_plus').nth(0)
+        const collapsePageIcon = Selector('img.icon_minus').nth(0)
+        const expandChildIcon = Selector('.icon_plus').nth(1)
+        const collapseChildIcon = Selector('.icon_minus').nth(1)
+
+        await t
+            .click(expandPageIcon)
+            .expect(collapseChildIcon.exists).ok('it passed')
+            .wait(2000)
+            .click(collapsePageIcon)
+            .expect(expandChildIcon.exists).ok('it passed')
+            .wait(2000)
+    })
+
