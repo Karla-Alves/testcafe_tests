77e169bdcdc4ca69d396a7da0db2c91e31f6869b
Karla Alves <karlaayeska12@gmail.com>
2018-06-21 16:10:34 -0600

    adding artifact

--- team_board/swimlane.js
+++ team_board/swimlane.js
@@ -8,7 +8,7 @@ var login = async t => {
         .click('#login-button')        
 }
     
-fixture `Team Board`
+fixture `Team Board Swimlane options`
     .page(`https://karla0.testn.f4tech.com/#/228928406146d/teamboard`)  
     .beforeEach(login)
     // beforeEach(async t => {
@@ -24,7 +24,11 @@ test('Team Board Swimlane', async t =>{
     const toolbarTray = Selector('#TOOLBAR_GROUP_BY_TRAY')
     const groupByDropdown = Selector('.chr-ToolbarGroupByTray-groupByField .smb-DropdownList')
     const groupByDropdownOptionsBlocked = Selector('.chr-ToolbarGroupByTray-groupByField .smb-DropdownList-list .smb-DropdownItem').nth(1)
-
+    const addNewButton = Selector('#TOOLBAR_ADD_NEW_TRAY-trigger .chr-ToolbarAddNewButton-addNewButton')
+    const workItemTypes = Selector('.chr-ToolbarAddNewTray-field .chr-ToolbarAddNewTray-workItemTypeDropdown')
+    const workItemTypesDropdownTestSet = Selector('.smb-DropdownList-listContentWrapper ,smb-DropdownItem')
+    const nameBoxAddNew = Selector('.chr-ToolbarAddNewTray-nameInput .smb-TextInput')
+    const createButton = Selector('.chr-ToolbarAddNewTray-buttonField .smb-Button-contents').nth(0)
 
     await t
         if(await toolbarTray.exists){
@@ -37,6 +41,14 @@ test('Team Board Swimlane', async t =>{
                 .click(groupByDropdownOptionsBlocked)
                 .wait(200)
                 .expect({span:'Blocked'}).eql({span:'Blocked'}, 'this assertion will pass')
+                //adding a new artifact
+                .click(addNewButton)
+                .click(workItemTypes)
+                .click(workItemTypesDropdownTestSet)
+                .typeText(nameBoxAddNew, 'Test Set One')
+                .click(createButton)
+                .wait(200)
+
         }
     
         
