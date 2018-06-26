ea91835561705446497df819bd07e8f4a5382eff
Karla Alves <karlaayeska12@gmail.com>
2018-06-26 13:51:55 -0600

    checking work views

--- team_board/swimlane_name_change_from_group_by.js
+++ team_board/swimlane_name_change_from_group_by.js
@@ -19,7 +19,7 @@ fixture `Team Board Swimlane options`
         await t.eval(() => localStorage.clear());
     })
 
-test('Team Board Group By button name change to Swimlanes and Group by name change to SWIMLANE BY', async t =>{
+test('Team Board Group By button name change to Swimlanes and Group by name change to SWIMLANE BY("S161324")', async t =>{
     const groupByButton = Selector('#TOOLBAR_GROUP_BY_TRAY-trigger .chr-ToolbarGroupByButton-groupByButton .smb-Button-children')
     const toolbarTray = Selector('#TOOLBAR_GROUP_BY_TRAY')
     const swimlaneByButtonLabel = Selector('.chr-Tray-contents .chr-ToolbarGroupByTray-groupByField .smb-FieldLabel-text')
@@ -41,29 +41,44 @@ test('Team Board Group By button name change to Swimlanes and Group by name chan
 test('Work Views still has "Group By" button and "GROUP BY" label for dropdown', async t =>{
     const projectPicker = Selector('.chr-ProjectPickerSelect .chr-ProjectPickerSelect-select')
     const projectBrowser = Selector('.smb-DropdownList-footer .chr-ProjectPickerSelectContainer-footerButton')
-    const projectCheckbox = Selector('.smb-TableBody .smb-TableRow .smb-TableCell--checkbox') 
+    const projectCheckbox = Selector('.smb-DataTable-wrapper .smb-TableBody .smb-TableCell--checkbox .chr-ProjectPickerModal-checkbox') 
+    const projectHasBeenSelected = Selector('.chr-ToolbarMultiProjectFilterTray-fieldContainer .chr-ProjectPickerSelect .smb-Select-selectedValue')
+    //const projectCheckboxEnabled = Selector('.smb-DataTable-wrapper .smb-TableBody .smb-TableCell--checkbox .is-checked')
     const projectDoneButton = Selector('.smb-PanelFooter .chr-ProjectPickerModal-doneButton .smb-Button-children')
-    const workItemTypesPicker = Selector('.smb-DropdownList--bottom .smb-Select-trigger .smb-Select-placeholderText').nth(1)
+    const workItemTypesPicker = Selector('.chr-ToolbarMultiProjectFilterTray-fieldContainer .chr-MultiProjectFilterTrayTypeFilter .smb-Select-trigger')
     const workItemTypesDropdown = Selector('.smb-DropdownList-listContentWrapper .smb-DropdownList-list .smb-Checkbox').nth(2)
+    const workItemTypesBeenSelected = Selector('.chr-ToolbarMultiProjectFilterTray-fieldContainer .chr-MultiProjectFilterTrayTypeFilter .smb-Select-selectedValue')
     const groupByButton = Selector('#TOOLBAR_GROUP_BY_TRAY-trigger .chr-ToolbarGroupByButton-groupByButton .smb-Button-contents .smb-Button-children')
     const groupByButtonLabel = Selector('.chr-Tray-contents .chr-ToolbarGroupByTray-groupByField .smb-FieldLabel-text')
+    const toolbarTray = Selector('#TOOLBAR_GROUP_BY_TRAY')
 
     await t
         .navigateTo(`https://karla0.testn.f4tech.com/#/228928406146d/workviews`)
         .wait(2000)
-        //choosing a Project
-        .click(projectPicker)
-        .click(projectBrowser)
-        .click(projectCheckbox)
-        .click(projectDoneButton)
 
-        //chosing a Work Item Type
-        .click(workItemTypesPicker)
-        .click(workItemTypesDropdown)
+        //If a project is picked do nothing, otherwise pick a project
+        if(await projectHasBeenSelected.exists){
+            await t
+                //.click(projectPicker)
+        }else{
+            await t
+                .click(projectPicker)
+                .click(projectBrowser)
+                .click(projectCheckbox)
+                .click(projectDoneButton)
+        }
+        
+        //If a work item is already chosen, do nothing, otherwise pick a work item
+        if(await workItemTypesBeenSelected.exists){
+            await t
+                //.click(workItemTypesPicker)
+        }else{
+            await t
+                .click(workItemTypesPicker)
+                .click(workItemTypesDropdown)
+        } 
 
         //checking Group By button
-        .expect(groupByButton.textContent).eql('Group By')
-
         if(await toolbarTray.exists){
             await t
                 .expect(groupByButton.textContent).eql('Group By')         
